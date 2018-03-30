87103796b7dcd72bc264a6510d9c0d0c18efc827
MaxMedia-Apps <max.kievits@gmail.com>
2018-03-30 22:39:05 +0200

    Final Build 30/03/18
    Final Build 30/03/18

--- Underside/Assets/Scripts/pauseMenu.cs
+++ Underside/Assets/Scripts/pauseMenu.cs
@@ -20,7 +20,7 @@ public class pauseMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown(KeyCode.P)) {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
             if(gameIsPaused) {
                 gameResume();
             } else {
